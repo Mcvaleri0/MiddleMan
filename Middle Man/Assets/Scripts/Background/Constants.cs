7777aed4d231037a1d56b559b0adfd7e066bae7e
Miguel <miguel.valerio@tecnico.ulisboa.pt>
2021-08-04 16:15:07 +0100

    still not reading deeplinks when sent to background

--- Middle Man/Assets/Scripts/Background/Constants.cs
+++ Middle Man/Assets/Scripts/Background/Constants.cs
@@ -11,5 +11,7 @@ namespace Background
         public const string UNITY_PLAYER_ID = "com.unity3d.player.UnityPlayer";
         public const string ACTIVITY = "currentActivity";
         public const string TASK_TO_BACK = "moveTaskToBack";
+        public const string SET_TRANSLUCENT = "setTranslucent";
+        public const string SET_VISIBLE = "setVisible";
     }
 }
